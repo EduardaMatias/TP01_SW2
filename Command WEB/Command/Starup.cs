f4413268ba676368a2e9191c4ae679b7deddd55f
EduardaMatias <evcm2003@gmail.com>
2023-08-10 22:19:10 -0300

    Create web

---
+++ Command WEB/Command/Starup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace Command
+{
+    internal class Starup
+    {
+       public void Configure(IApplicationBuilder app)
+        {
+            var builder = new RouteBuilder(app);
+            builder.MapRoute("/NomeLivros", NomesLivros);
+            builder.MapRoute("/Autores");
+            builder.MapRoute("/DadosLivro");
+            builder.MapRoute("/livro/ApresentarLivro/{nome}/");
+        }
+        public Task NomesLivros(HttpContext context)
+
+    }
+}
